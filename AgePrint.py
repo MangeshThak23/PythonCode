4aae714507e515bd65fbc2ebd6055f694fd6afbf
MangeshThak23 <mangesht.2324@gmail.com>
2026-06-12 15:39:12 +0530

    Assignment 1

---
+++ AgePrint.py
@@ -0,0 +1,11 @@
+#9. Write a program to take user’s name and age and display:
+#Hello <name>, you will turn <age+1> next year.
+
+name=input("Enter Name: ")
+age=int(input("Enter Age: "))
+print("Hello "+ name + ", you will turn "+str(age+1)+" next year.")
+
+#Output
+#Enter Name: XYZ
+#Enter Age: 20
+#Hello XYZ, you will turn 21 next year.
---
+++ PythonBuiltInFunction.py
@@ -0,0 +1,15 @@
+#1.Write a program to display:
+#Value
+#Type
+#Memory address
+#for a variable using appropriate built-in functions.
+
+x=10
+print("Value : ", (x))
+print("Type : ", type(x))
+print("Memory Address :", id(x))
+
+#output
+#Value :  10
+#Type :  <class 'int'>
+#Memory Address : 140721176003784
