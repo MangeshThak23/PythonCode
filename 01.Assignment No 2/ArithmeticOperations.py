8ba0e919c99fe185dfcbf0f19c6e0fe310c807aa
MangeshThak23 <mangesht.2324@gmail.com>
2026-06-12 23:14:00 +0530

    Assignment 1

---
+++ 01.Assignment No 2/ArithmeticOperations.py
@@ -0,0 +1,21 @@
+#6. Write a program that accepts two numbers #from user and prints their:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+
+num1=int(input("Enter first number : "))
+num2=int(input("Enter second number : "))
+
+print("Addition :", num1 + num2)
+print("Substraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division :", num1 / num2)
+
+#Output
+#Enter first number : 10
+#Enter second number : 20
+#Addition : 30
+#Substraction : -10
+#Multiplication : 200
+#Division : 0.5
